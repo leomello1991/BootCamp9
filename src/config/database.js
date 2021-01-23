f04ba03848f3984192d529aa049417e0cecc8eef
leonardo de Melo <leonardomelo60260@gmail.com>
2021-01-23 01:06:11 -0300

    fineshed ambiente e conceitos

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
